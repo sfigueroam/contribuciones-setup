36f3940b9dbfe7e61944496cf6dd90f5a9d00035
rvillegas <rvillegas@tgr.cl>
2020-01-06 11:32:45 -0300

    Se argrega TLSv1.2 a CloudFront

--- runtime/resource/cloudfront/cloudfront.tf
+++ runtime/resource/cloudfront/cloudfront.tf
@@ -90,6 +90,7 @@ resource "aws_cloudfront_distribution" "cloudfrontDistributionS3Front" {
   viewer_certificate {
     acm_certificate_arn = "${var.acmCertificateArn}"
     ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
   }
 
   restrictions {
